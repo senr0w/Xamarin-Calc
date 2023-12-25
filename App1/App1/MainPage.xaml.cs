3504010230e4929397d7cfba4296b7c75a57e1f5
GoodNight <129828409+Dasha43244@users.noreply.github.com>
2023-12-25 14:56:44 +0700

    load

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+            int currentState = 1;
+            string myoperator;
+            double firstNumber, secondNumber;
+
+            public MainPage()
+            {
+                InitializeComponent();
+                OnClear(this, null);
+            }
+
+            void OnSelectNumber(object sender, EventArgs e)
+            {
+                Button button = (Button)sender;
+
+
+                string pressed = button.Text;
+      
+                if (this.resultText.Text == "0" || currentState < 0)
+                {
+                    this.resultText.Text = "";
+
+                    if (currentState < 0) 
+                        currentState *= -1;
+                }
+
+                this.resultText.Text += pressed;
+
+                double number;
+                if (double.TryParse(this.resultText.Text, out number))
+                {
+                    this.resultText.Text = number.ToString("N0");
+                    if (currentState == 1)
+                    {
+                        firstNumber = number;
+                    }
+                    else
+                    {
+                        secondNumber = number;
+                    }
+                }
+            }
+
+            void OnSelectOperator(object sender, EventArgs e)
+            {
+                currentState = -2;
+                Button button = (Button)sender;
+                string pressed = button.Text;
+                myoperator = pressed;
+            }
+
+            void OnClear(object sender, EventArgs e)
+            {
+                firstNumber = 0;
+                secondNumber = 0;
+                currentState = 1;
+                this.resultText.Text = "0";
+            }
+
+            void OnPercentage(object sender, EventArgs e) 
+            {
+
+                if ((currentState == -1) || (currentState == 1))
+                {
+                    var result = firstNumber / 100;
+                    this.resultText.Text = result.ToString();
+                    firstNumber = result;
+                    currentState = -1;
+
+                }
+
+
+            }
+            void OnCalculate(object sender, EventArgs e) 
+            {
+                if (currentState == 2)
+                {
+                    var result = OperatorHelper.Calculate(firstNumber, secondNumber, myoperator);
+
+                    this.resultText.Text = result.ToString();
+                    firstNumber = result;
+                    currentState = -1;
+                }
+            }
+            void OnSquareRoot(object sender, EventArgs e) 
+            {
+                if ((currentState == -1) || (currentState == 1))
+                {
+                    var result = Math.Sqrt(firstNumber);
+
+                    this.resultText.Text = result.ToString();
+                    firstNumber = result;
+                    currentState = -1;
+                }
+            }
+
+
+            private void squareclicked(object sender, EventArgs e)
+            {
+
+                if ((currentState == -1) || (currentState == 1))
+                {
+                    var result = firstNumber * firstNumber;
+                    this.resultText.Text = result.ToString();
+                    firstNumber = result;
+                    currentState = -1;
+                }
+            }
+
+        }
+    }
+
---
+++ App1/App1/OperatorHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public static class OperatorHelper
+    {
+
+
+
+        public static double Calculate(double value1, double value2, string myoperator)
+        {
+            double result = 0;
+            switch (myoperator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+
+            }
+            return result;
+
+        }
+
+
+    }
+}
